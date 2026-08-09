f52df00b48cdaf88c507dc4c90e8b2dc7e806ca1
xiaye <xiaye@msn.com>
2026-08-09 08:52:40 +0000

    feat: 发布AI代笔公告

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -4,6 +4,13 @@ export const announcementConfig: AnnouncementConfig = {
 	title: "公告",
 	items: [
 		{
+			tag: "公告",
+			title: "公告",
+			content: "本站的文章由AI代笔，如有疏漏请在评论区补充或者联系站长",
+			time: "2026-08-09",
+			sort: 0,
+		},
+		{
 			tag: "欢迎",
 			title: "欢迎",
 			content: "欢迎来到夜翼玖的博客",
